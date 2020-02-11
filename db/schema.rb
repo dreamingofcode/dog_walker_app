12ea77aca7a27a95280f3b105c48dfb6c608933a
learn <github email address>
2020-02-11 11:17:16 -0800

    user model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_182642) do
+ActiveRecord::Schema.define(version: 2020_02_11_190242) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2020_02_11_182642) do
     t.integer "rating"
     t.string "comment"
     t.integer "appointment_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
