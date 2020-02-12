246721c581b952ac6eae19b9d5fbe72fe5108fc5
Gonzalez32 <3232rose@gmail.com>
2020-02-11 18:10:01 -0600

    walker modified

---
+++ app/controllers/walkers_controller.rb
@@ -0,0 +1,14 @@
+class WalkersController < ApplicationController
+    # def index 
+    #     @walkers = Walker.all 
+    # end     
+
+    def show 
+        @walker = Walker.find(params[:id])
+        # @appointments = @walker.appointments 
+        @appointment = Appointment.find_by(walker_id: params[:id])
+        @user = Appointment.find_by(walker_id: params[:id]).user
+        @dogs = Dog.find_by(user_id: @user)  
+        # @appointments = Walker.find(params[:id])
+    end
+end
