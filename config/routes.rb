1e50dcb64df610760aa4a7f9145a57db45b94db8
Cesar <cesarmota@ymail.com>
2020-02-12 11:45:07 -0600

    dog form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :walkers, only: [:show]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :reviews, only: [:create, :new, :show]
+  resources :dogs, only: [:new,:create]
 
   root 'application#hello'
   get '/login' => 'sessions#new'
@@ -14,3 +15,32 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
 
 end
+
+
+# Prefix Verb   URI Pattern                                                                              Controller#Action
+# appointments GET    /appointments(.:format)                                                                  appointments#index
+#              POST   /appointments(.:format)                                                                  appointments#create
+# new_appointment GET    /appointments/new(.:format)                                                              appointments#new
+# edit_appointment GET    /appointments/:id/edit(.:format)                                                         appointments#edit
+#  appointment GET    /appointments/:id(.:format)                                                              appointments#show
+#              PATCH  /appointments/:id(.:format)                                                              appointments#update
+#              PUT    /appointments/:id(.:format)                                                              appointments#update
+#              DELETE /appointments/:id(.:format)                                                              appointments#destroy
+#       walker GET    /walkers/:id(.:format)                                                                   walkers#show
+#        users POST   /users(.:format)                                                                         users#create
+#     new_user GET    /users/new(.:format)                                                                     users#new
+#    edit_user GET    /users/:id/edit(.:format)                                                                users#edit
+#         user GET    /users/:id(.:format)                                                                     users#show
+#              PATCH  /users/:id(.:format)                                                                     users#update
+#              PUT    /users/:id(.:format)                                                                     users#update
+#      reviews POST   /reviews(.:format)                                                                       reviews#create
+#   new_review GET    /reviews/new(.:format)                                                                   reviews#new
+#       review GET    /reviews/:id(.:format)                                                                   reviews#show
+#         dogs POST   /dogs(.:format)                                                                          dogs#create
+#      new_dog GET    /dogs/new(.:format)                                                                      dogs#new
+#         root GET    /                                                                                        application#hello
+#        login GET    /login(.:format)                                                                         sessions#new
+#              POST   /login(.:format)                                                                         sessions#create
+#       logout POST   /logout(.:format)                                                                        sessions#destroy
+#       signup GET    /signup(.:format)                                                                        users#new
+#              POST   /signup(.:format)   
