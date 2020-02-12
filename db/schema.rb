561d4e0bea30f5a92cbbc0e9f67d5a4d246087f8
learn <github email address>
2020-02-12 11:35:58 -0800

    removing columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_160154) do
+ActiveRecord::Schema.define(version: 2020_02_12_193357) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer "user_id"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2020_02_12_160154) do
 
   create_table "walkers", force: :cascade do |t|
     t.string "name"
-    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
