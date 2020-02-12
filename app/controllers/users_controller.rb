d99c5e580cecae6743ee9c80a7687bf8e3ff8aa7
Cesar <cesarmota@ymail.com>
2020-02-12 10:11:50 -0600

    user showpage in the works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,13 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
-        session[:user_id] = @user.user_id
-        redirect_to user_path(user)
+        user = User.create(user_params)
+        session[:id] = user.id 
+        redirect_to user_path(user.id) #if @user.valid?
+       
+            # flash[:message] ="you dumb hoe!"
+            # redirect_to new_user_path
+        
     end
 
     def show
